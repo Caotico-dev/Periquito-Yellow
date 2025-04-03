1351e2e6912e9af4d4b07654cb8b5af2cd0aa20f
Caotico-dev <danieljosez2003@gmail.com>
2025-04-03 01:28:44 -0400

    add SimpleCard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import Card from "./Card.jsx";
 import ViewPage from "./ViewPage.jsx";
 import PreviewPage from "./PreviewPage.jsx";
+import SimpleCard from "./SimpleCard.jsx";
 
 function Home() {
   return (
     <div className="container">
       <Card />
+      <SimpleCard />      
       <ViewPage title={"Recomendaciones"}>
         <PreviewPage />
         <PreviewPage />
---
+++ src/components/SimpleCard.jsx
@@ -0,0 +1,43 @@
+function SimpleCard({
+  title = "Simple Card",
+  text = [
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  ],
+  subtopics = [
+    {
+      title: "Subtopics",
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      title: "Subtopics",
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      title: "Subtopics",
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+  ],
+}) {
+  return (
+    <section className="simple-card border">
+      <h2 className="simple-card-title">{title}</h2>
+      <div className="sc-container-text">
+        {text.map((text, index) => (
+          <p className="simple-card-text" key={index}>
+            {text}
+          </p>
+        ))}
+      </div>
+      <div className="sc-container-subtopics">
+        {subtopics.map((subtopics, index) => (
+          <div className="sc-subtopic" key={index}>
+            <h3 className="sc-subtopics-title">{subtopics.title}</h3>
+            <p className="sc-subtopics-text">{subtopics.text}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+export default SimpleCard;
