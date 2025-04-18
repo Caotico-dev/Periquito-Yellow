470d451c77e3ca5a9f37d6633b16c8779435d85e
Caotico-dev <danieljosez2003@gmail.com>
2025-04-18 00:08:14 -0400

    Add ImagesViewer commponents

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,15 @@ import ViewPage from "./ViewPage.jsx";
 import PreviewPage from "./PreviewPage.jsx";
 import SimpleCard from "./SimpleCard.jsx";
 import SpacialCard from "./SpacialCard.jsx";
+import ImagesViewer from "./ImagesViewer.jsx";
 
 function Home() {
   return (
     <div className="container">
       <Card />
       <SpacialCard/>
-      <SimpleCard />      
+      <SimpleCard />   
+      <ImagesViewer/>   
       <ViewPage title={"Recomendaciones"}>
         <PreviewPage />
         <PreviewPage />
